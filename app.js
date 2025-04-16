648ed3085ce24ac865c72b65b0c68edc21c9b194
abbiebabu <abhaymaskey12@gmail.com>
2025-04-16 14:51:54 +0545

    added all currency codes in the options

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
 
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.value = currCode;
+    newOption.innerText = currCode;
+    select.append(newOption);
+  }
+}
